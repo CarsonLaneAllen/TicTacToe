7bdb2fcaaffac4f4b339e58351b4bb9e7a85d348
Graham Allen <grahamtallen94@gmail.com>
2016-04-21 20:05:40 -0700

    Refactored main.js, more efficient

--- src/js/main.js
+++ src/js/main.js
@@ -7,47 +7,87 @@
 var grid = [[], [""], [""], [""]];
 
 
-
 $.fn.child = function (s) {
     return $(this).children(s);
 };
 
-
-// To add a listener to check the score, just define the function somewhere else and add it to the #whole listneer
-
-var startGame = function () {
-    turn = Math.random() < 0.5 ? false : true;
-    if (!turn) {
-        xTurn();
-    } else if (turn) {
-        $('.target0').removeAttr('style');
-        oTurn();
-    }
-    clickHandler();
-    $('#reset').attr('style', 'display: inline');
-    $('#startGame').attr('style', 'display: none');
-    $('#startGame').closest('.topsquare').attr('onclick', 'reset()');
-};
-
-
+var re$et = $('#reset');
+var start = $('#startGame');
+var xDiv = $('.targetX');
+var oDiv = $('.targetO');
 var turn;
 var letter, klass;
+var all;
 
 
-function oTurn() {
-    $(".targetO").fadeIn('slow');
-    $(".targetX").fadeOut('slow');
-    letter = "O";
-    klass = 'fa fa-5x fa-circle-o';
+// When 'Begin' is clicked, set turn to true or false, then activate the corresponding funci
+
+var startGame = function () {
+
+    turn = Math.random() < 0.5 ? false : true;
+
+    if (!turn) {
+        switchTurn('X', "fa fa-5x fa-times", xDiv);
+    } else if (turn) {
+        switchTurn('O', "fa fa-5x fa-circle-o", oDiv);
+    }
+
+    $('.fa').attr('class', '');
+
+    clickHandler();
+
+    re$et.attr('style', 'display: inline');
+    start.attr('style', 'display: none');
+    start.closest('.topsquare').attr('onclick', 'reset()');
+
+    $('.square').child().fadeIn('slow');
+};
+
+function switchTurn(l, cLass, div) {
+    div.fadeToggle('slow');
+    letter = l;
+    klass = cLass;
     turn = !turn;
 }
-function xTurn() {
-    $(".targetX").fadeIn('slow');
-    $(".targetO").fadeOut('slow');
-    letter = "X";
-    klass = 'fa fa-5x fa-times';
-    turn = !turn;
+
+function clickHandler() {
+
+    $('.square').click(function () {
+
+        if ($(this).data('value') === "") {
+
+            $(this).child().attr('class', klass);
+            $(this).data('value', letter);
+            if(horWin() || vertWin() || diags()) {
+                return
+            };
+
+            if (!turn) {
+                switchTurn('X', "fa fa-5x fa-times", xDiv);
+                oDiv.fadeToggle('slow')
+            } else if (turn) {
+                switchTurn('O', "fa fa-5x fa-circle-o", oDiv);
+                xDiv.fadeToggle('slow')
+            }
+        }
+    })
 }
+
+// Reset the board
+
+function reset() {
+    $('.fa').fadeOut('slow');
+    $('.square').data('value', '');
+    start.attr('style', 'display: inline');
+    re$et.attr('style', 'display: none');
+    xDiv.fadeOut('fast');
+    oDiv.fadeOut('fast');
+    start.closest('.topsquare').attr('onclick', 'startGame()');
+}
+
+
+// Create a function that checks for a win
+
 
 for (i = 1; i < 4; i++) {
     for (j = 1; j < 4; j++) {
@@ -57,69 +97,59 @@ for (i = 1; i < 4; i++) {
     }
 }
 
-function clickHandler() {
-    $('.square').click(function () {
-        if ($(this).data('value') === "") {
-            $(this).child().attr('class', klass);
-            $(this).data('value', letter);
-            if (!turn) {
-                xTurn();
-            } else if (turn) {
-                oTurn();
-            }
-        } else {
-            console.log('taken');
-        }
-        horWin();
-        vertWin();
-        fullBoard();
-    })
-}
-
-// Reset the board
-
-function reset() {
-    for (i = 1; i < 4; i++) {
-        for (j = 1; j < 4; j++) {
-            grid[i][j].child().attr('class', '');
-            grid[i][j].data('value', '')
-        }
-    }
-    $('#startGame').attr('style', 'display: inline');
-    $('#reset').attr('style', 'display: none');
-    $(".targetX").fadeOut('fast');
-    $(".targetO").fadeOut('fast');
-    $('#startGame').closest('.topsquare').attr('onclick', 'startGame()');
-}
-
-
-// Create a function that checks for a win
-
 function inARow(one, two, three) {
-    if (one.data('value') === two.data('value')) {
-        if (two.data('value') === three.data('value') && one.data('value') === three.data('value')) {
-            if (one.data('value') !== "") {
-                $('.fa').fadeOut('slow');
-                function fadeOne() {
-                    one.find('.fa').fadeIn('slow');
-                }
-                function fadeTwo() {
-                    two.find('.fa').fadeIn('slow')
-                }
-                function fadeThree() {
-                    three.find('.fa').fadeIn('slow')
-                }
-                winner(one);
-                setTimeout(fadeOne, 200);
-                setTimeout(fadeTwo, 800);
-                setTimeout(fadeThree, 1200);
-                function fadeAll() {
-                    $('.fa').fadeIn('slow');
-                }
-                setTimeout(fadeAll, 2000)
-            }
-        }
+    var won = one.data('value');
+    var too = two.data('value');
+    var tree = three.data('value');
+
+    all = won + too + tree;
+
+    switch(all) {
+        case 'XXX':
+            return gameWin();
+        case 'OOO':
+            return gameWin();
     }
+
+    function gameWin() {
+        $('.fa').fadeOut('slow');
+        function fadeOne() {
+            one.find('.fa').fadeIn('slow');
+        }
+
+        function fadeTwo() {
+            two.find('.fa').fadeIn('slow')
+        }
+
+        function fadeThree() {
+            three.find('.fa').fadeIn('slow')
+        }
+
+        winner(one);
+        setTimeout(fadeOne, 200);
+        setTimeout(fadeTwo, 800);
+        setTimeout(fadeThree, 1200);
+        function fadeAll() {
+            $('.fa').fadeIn('slow');
+        }
+
+        setTimeout(fadeAll, 2000);
+
+        $('.square').unbind('click');
+        return true;
+    }
+
+    // First iteration of a check for a win:
+
+
+//    if (won === two.data('value')) {
+//        if (two.data('value') === three.data('value') && won === three.data('value')) {
+//            if (won !== "") {
+//
+//            }
+//        }
+//    }
+    
 }
 
 function winner(a) {
@@ -141,27 +171,21 @@ function winner(a) {
 // inARow(grid[1][1], grid[2][1], grid[3][1])
 
 function horWin() {
-    inARow(grid[1][1], grid[1][2], grid[1][3]);
-    inARow(grid[2][1], grid[2][2], grid[2][3]);
+    return inARow(grid[1][1], grid[1][2], grid[1][3]) ||
+    inARow(grid[2][1], grid[2][2], grid[2][3]) ||
     inARow(grid[3][1], grid[3][2], grid[3][3])
 }
 
 function vertWin() {
-    inARow(grid[1][1], grid[2][1], grid[3][1]);
-    inARow(grid[1][2], grid[2][2], grid[3][2]);
+    return inARow(grid[1][1], grid[2][1], grid[3][1]) ||
+    inARow(grid[1][2], grid[2][2], grid[3][2]) ||
     inARow(grid[1][3], grid[2][3], grid[3][3])
 }
 
-// Check if the board is full
+function diags() {
+    return inARow(grid[1][1], grid[2][2], grid[3][3]) ||
+        inARow(grid[1][3], grid[2][2], grid[3][1])
+}
 
-var fullBoard = function() {
-//    for (i = 1; i < 4; i++) {
-//        for (j = 1; j < 4; j++) {
-//            if (grid[i][j].data('value') === "") {
-//                return;
-//            } else {
-//                console.log('gameover')
-//            }
-//        }
-//    }
-};
+
+
